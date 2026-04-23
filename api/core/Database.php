3db069897df674c8790046078e78652772506031
Emma Dochi <emmadochi@gmail.com>
2026-04-23 21:42:26 +0100

    Final hardening of API responses for production stability

--- api/core/Database.php
+++ api/core/Database.php
@@ -17,7 +17,7 @@ class Database {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = @new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8mb4");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
--- api/index.php
+++ api/index.php
@@ -4,8 +4,8 @@
  * Handles all requests to /api/v1/
  */
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 // Basic CORS headers
 header("Access-Control-Allow-Origin: *");
