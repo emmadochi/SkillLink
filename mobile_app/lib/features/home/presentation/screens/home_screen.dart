1b8d3d5c49fcb5ed8f74ff97bd6b58207aece7bc
Emma Dochi <emmadochi@gmail.com>
2026-04-24 09:48:46 +0100

    Final Touch: Added currency formatting to Home Screen and verified build stability

--- mobile_app/lib/features/home/presentation/screens/home_screen.dart
+++ mobile_app/lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 import '../../../../core/constants/app_constants.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_typography.dart';
